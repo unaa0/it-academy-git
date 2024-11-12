85d5a55594bf50661e1ef14c5dce739e92c36e54
unaa0 <anastasiyamik02@gmail.com>
2024-11-12 15:01:26 +0300

    Third Commit

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 print("I’m planning to learn git conflicts resolution")
+name = input("Please enter your name: ")
+surname = input("Please enter your surname: ")
+print(f"Hi, {name} {surname}!", format(name, surname))
