39c93fc3f528e39b2ebf43d89363d5a433ec1def
unaa0 <anastasiyamik02@gmail.com>
2024-11-30 02:00:51 -0800

    Removed hw3 folder from this branch

--- Homework_3/task1.py
+++
@@ -1,5 +0,0 @@
-def fizz_buzz(num):
-    result = ["FizzBuzz" if x % 5 == 0 and x % 3 == 0 else "Buzz" if x % 5 == 0 else "Fizz" if x % 3 == 0 else x for x in range(1, 101)]
-    return result
-
-print(fizz_buzz(100))
--- Homework_3/task5.py
+++
@@ -1,8 +0,0 @@
-def dup_elements(list1):
-    duplicates = []
-    for element in list1:
-        if element not in duplicates:
-            duplicates.append(element)
-    return duplicates
-
-print(dup_elements([1, 2, "a", 3, 1, 2, "a", 4, 5, "b"]))
