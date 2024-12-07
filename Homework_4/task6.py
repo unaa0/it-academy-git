a5f06a7b1d77f892c17b4dcb6aa7f27fb2986ee4
unaa0 <anastasiyamik02@gmail.com>
2024-12-07 05:05:47 -0800

    Completed task1 with txt file, removing unnecessary files

--- Homework_4/task6.py
+++
@@ -1,6 +0,0 @@
-def count_words(text):
-    text = text.split()
-    return f"{text} => There are {len(text)} words"
-
-print(count_words("Word #$%,   jwsgvr !@#$%^ \n textetxtex text"))
-print(count_words("Another, Sen\nte\nnce   \nwow\t cat 3456 345&&"))
